dc2b145f39f39c7feb9e17933279e1f28020602c
rafaelmazzoni25-sys <rafaelmazzoni25@gmail.com>
2025-11-13 14:29:31 -0300

    Revert "Refine UE5 remake roadmap documentation"

--- Dados/07_WorldAndSpawns.cpp
+++ Dados/07_WorldAndSpawns.cpp
@@ -1,1 +1,109 @@
-/*
+// Etapa 7: Mundo Aberto, Spawns e Bosses
+// Configura geração de monstros, gerenciador de instâncias e eventos de mundo.
+
+#include "GameFramework/Actor.h"
+#include "Engine/World.h"
+#include "TimerManager.h"
+#include "Components/BoxComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+
+USTRUCT(BlueprintType)
+struct FSpawnEntry
+{
+GENERATED_BODY()
+
+UPROPERTY(EditAnywhere, BlueprintReadWrite) TSubclassOf<AActor> MonsterClass;
+UPROPERTY(EditAnywhere, BlueprintReadWrite) int32 Quantity = 3;
+UPROPERTY(EditAnywhere, BlueprintReadWrite) float RespawnTime = 30.f;
+};
+
+UCLASS()
+class ASpawnVolume : public AActor
+{
+GENERATED_BODY()
+
+public:
+ASpawnVolume()
+{
+Volume = CreateDefaultSubobject<UBoxComponent>(TEXT("Volume"));
+SetRootComponent(Volume);
+}
+
+virtual void BeginPlay() override
+{
+Super::BeginPlay();
+ScheduleRespawn();
+}
+
+void SpawnMonsters()
+{
+for (const FSpawnEntry& Entry : SpawnList)
+{
+for (int32 i = 0; i < Entry.Quantity; ++i)
+{
+const FVector Location = UKismetMathLibrary::RandomPointInBoundingBox(Volume->Bounds.Origin, Volume->Bounds.BoxExtent);
+GetWorld()->SpawnActor<AActor>(Entry.MonsterClass, Location, FRotator::ZeroRotator);
+}
+}
+ScheduleRespawn();
+}
+
+private:
+void ScheduleRespawn()
+{
+GetWorldTimerManager().SetTimer(RespawnTimer, this, &ASpawnVolume::SpawnMonsters, GlobalRespawnTime, false);
+}
+
+public:
+UPROPERTY(EditAnywhere, BlueprintReadWrite)
+TArray<FSpawnEntry> SpawnList;
+
+UPROPERTY(EditAnywhere, BlueprintReadWrite)
+float GlobalRespawnTime = 30.f;
+
+private:
+UPROPERTY()
+UBoxComponent* Volume;
+
+FTimerHandle RespawnTimer;
+};
+
+// Eventos de mundo e bosses
+UCLASS()
+class AWorldEventManager : public AActor
+{
+GENERATED_BODY()
+
+public:
+void StartBossEvent()
+{
+// Ativar cutscene, spawnar boss, bloquear portais
+OnBossEventStarted.Broadcast();
+}
+
+void FinishBossEvent(bool bSuccess)
+{
+if (bSuccess)
+{
+OnBossEventSucceeded.Broadcast();
+}
+else
+{
+OnBossEventFailed.Broadcast();
+}
+}
+
+public:
+DECLARE_MULTICAST_DELEGATE(FOnBossEventStarted);
+DECLARE_MULTICAST_DELEGATE(FOnBossEventSucceeded);
+DECLARE_MULTICAST_DELEGATE(FOnBossEventFailed);
+
+FOnBossEventStarted OnBossEventStarted;
+FOnBossEventSucceeded OnBossEventSucceeded;
+FOnBossEventFailed OnBossEventFailed;
+};
+
+// Notas:
+// - Combine com DataAssets de monstros e drop tables (arquivo 06).
+// - Integrar com sistemas de matchmaking para bosses instanciados.
--- Dados/09_NetworkingAndPersistence.cpp
+++ Dados/09_NetworkingAndPersistence.cpp
@@ -1,1 +1,117 @@
-/*
+// Etapa 9: Rede, Persistência e Escalabilidade
+// Implementa componentes para replicação, instâncias e salvamento persistente.
+
+#include "GameFramework/GameStateBase.h"
+#include "GameFramework/PlayerState.h"
+#include "GameFramework/SaveGame.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "Net/UnrealNetwork.h"
+
+UCLASS()
+class ARemakePlayerState : public APlayerState
+{
+GENERATED_BODY()
+
+public:
+virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override
+{
+Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+DOREPLIFETIME(ARemakePlayerState, Level);
+DOREPLIFETIME(ARemakePlayerState, Experience);
+}
+
+void AddExperience(int32 Amount)
+{
+Experience += Amount;
+OnRep_Experience();
+}
+
+protected:
+UFUNCTION()
+void OnRep_Experience()
+{
+// Atualizar HUD
+}
+
+public:
+UPROPERTY(ReplicatedUsing=OnRep_Experience)
+int32 Experience;
+
+UPROPERTY(Replicated)
+int32 Level;
+};
+
+UCLASS()
+class URemakePersistenceSubsystem : public UGameInstanceSubsystem
+{
+GENERATED_BODY()
+
+public:
+virtual void Initialize(FSubsystemCollectionBase& Collection) override
+{
+Super::Initialize(Collection);
+LoadGlobalConfig();
+}
+
+void SavePlayer(const FString& PlayerId, const FPlayerSaveData& Data)
+{
+// Serializa para arquivo ou serviço remoto
+}
+
+bool LoadPlayer(const FString& PlayerId, FPlayerSaveData& OutData)
+{
+// Recupera dados persistentes
+return true;
+}
+
+private:
+void LoadGlobalConfig()
+{
+// Lê arquivo JSON com configurações de servidores, shards, etc.
+}
+};
+
+USTRUCT(BlueprintType)
+struct FPlayerSaveData
+{
+GENERATED_BODY()
+
+UPROPERTY() int32 Level;
+UPROPERTY() int32 Experience;
+UPROPERTY() TArray<FInventorySlot> Inventory;
+UPROPERTY() TArray<FName> CompletedQuests;
+};
+
+UCLASS()
+class ARemakeGameState : public AGameStateBase
+{
+GENERATED_BODY()
+
+public:
+virtual void BeginPlay() override
+{
+Super::BeginPlay();
+if (HasAuthority())
+{
+StartWorldTick();
+}
+}
+
+void StartWorldTick()
+{
+GetWorldTimerManager().SetTimer(WorldTickHandle, this, &ARemakeGameState::HandleWorldTick, 5.f, true);
+}
+
+private:
+void HandleWorldTick()
+{
+// Atualiza eventos globais, ranking e sincroniza com servidor mestre
+}
+
+private:
+FTimerHandle WorldTickHandle;
+};
+
+// Notas:
+// - Combine com um backend dedicado (AWS, Azure etc) para persistência real.
+// - Configurar replication graph para grandes populações.
